1b87bbd85b413dd5f714e1dcc96d3ec4558aab12
shouko.yamashita <white_lemontea@yahoo.co.jp>
2021-03-11 14:53:30 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
-   get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
